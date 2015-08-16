3bb162f92e1b699fd47be878897203653c356b54
rgv <rajaganesh.v@gmail.com>
2015-08-16 12:10:26 -0400

    First test file commit.

---
+++ MyFiles.py
@@ -0,0 +1,13 @@
+__author__ = 'RajaganeshVanarajan'
+
+print("This is my first line of learning Python! Hello World!")
+
+for i in range(1,100):
+    print(i)
+else:
+    print(i)
+
+while(i<100):
+    print(i)
+    i=i+1
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PythonPractice
+
+# This is a repository with all my Python Practice exercises from my self-learning. 
